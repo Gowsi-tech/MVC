58fec4646864463ffffdfcfbf4132b01b8d654b6
Devigowsika UCA <devigowsika@unicornconnectedapps.com>
2025-02-19 17:05:24 +0530

    first commit

---
+++ login and register/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace login_and_register.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("/Identity/Account/Login");
+        }
+    }
+}
